0beb9f47ad3cdf32c2918ca4ff897b1c78310a22
Mathews Wednesday <q3dm0@yahoo.com>
2019-05-27 15:20:57 +0300

    изменения сервера

--- catalog/language/ru-ru/extension/shipping/free.php
+++ catalog/language/ru-ru/extension/shipping/free.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title']       = 'Бесплатная доставка';
-$_['text_description'] = 'Бесплатная доставка';
+$_['text_title']       = 'Предоплата';
+$_['text_description'] = 'Предоплата';
